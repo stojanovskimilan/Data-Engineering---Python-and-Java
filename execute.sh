454e8ad7a23051aded566b39ef935cbfed4e5682
Milan Stojanovski <60106991+stojanovskimilan@users.noreply.github.com>
2024-01-26 18:05:27 +0100

    Delete execute.sh

--- execute.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-c:; cd 'c:\Users\milan.stojanovski\Data Engineering - Python and Java'; & 'C:\Program Files\Java\jdk-19\bin\java.exe' '@C:\Users\MILAN~1.STO\AppData\Local\Temp\cp_cpwjxqerxu5982zjom1fdhpry.argfile' 'CsvToPostgres' >> test.txt
-
-cd Python
-
-ls
